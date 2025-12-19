00dbf40cbd69cfc14d1a538955bd4c671abb96a5
ekolmas <66930465+ekolmas@users.noreply.github.com>
2025-12-19 20:01:25 +0800

    191225 Removed unnecessary code
    Removed nodejs server testing route which was used to test python process triggering using nodeJs Express
    
    Removed testing.py file which was previously used to test NodeJs Express triggering python

--- Prototype/server.js
+++ Prototype/server.js
@@ -80,55 +80,6 @@ app.get("/download/:filename", (req, res) => {
   return res.download(filePath);
 });
 
-// Route to test python NodeJS integration
-app.get("/python_test/:testingParam", (req, res) => {
-  const testingParam = req.params.testingParam;
-
-  // spawn python process
-  const pythonProcess = spawn("python", ["scripts/testing.py", testingParam]);
-
-  let stdoutData = "";
-  let stderrData = "";
-
-  pythonProcess.stdout.on("data", (chunk) => {
-    stdoutData += chunk.toString();
-  });
-
-  pythonProcess.stderr.on("data", (chunk) => {
-    stderrData += chunk.toString();
-  });
-
-  pythonProcess.on("close", (code) => {
-    if (code !== 0) {
-      console.error("Python exited with code:", code);
-      console.error("Python stderr:", stderrData.trim());
-      return res.status(500).json({
-        ok: false,
-        error: "Python script failed",
-        code,
-        stderr: stderrData.trim(),
-      });
-    }
-
-    const lines = stdoutData
-      .split(/\r?\n/)
-      .map((l) => l.trim())
-      .filter(Boolean);
-
-    const returnedValue = lines.length ? lines[lines.length - 1] : "";
-
-    console.log(`NodeJS received the number ${returnedValue} from python`);
-
-    //send to front end
-    return res.json({
-      ok: true,
-      sentToPython: testingParam,
-      receivedFromPython: returnedValue,
-      pythonLogLines: lines,
-    });
-  });
-});
-
 // Start the server on port 3000
 app.listen(PORT, host, () => {
   console.log(`Server running at http://${host}:${PORT}/`);
